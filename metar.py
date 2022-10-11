56490bfbfc13e436be06d986105300892c2c77aa
Samuel Martinez <smohuff@gmail.com>
2022-10-11 01:23:35 -0500

    refactor

--- metar.py
+++ metar.py
@@ -3,15 +3,16 @@ import xmltodict
 
 hours = "1.5"
 
+# Receives a string of 4 character ICAOs separated by commas. Returns a list of ICAOs.
 def transformMetar(metarString) -> list:
-    metarString = metarString.upper()
-    metarString = metarString.replace(" ", "")
+    metarString = metarString.upper().replace(" ", "")
     metarList = metarString.split(",")
     for metar in metarList:
         if len(metar) != 4:
             return "invalid"
     return metarList
 
+# Receives a list of ICAOs. Returns a METAR XML string.
 def requestMetar(metarList):
     payload = {
         "datasource":"metars", "requestType": "retrieve", "format": "xml",
@@ -20,20 +21,22 @@ def requestMetar(metarList):
         }
     request = requests.get(
         "https://www.aviationweather.gov/adds/dataserver_current/httpparam",
-        params=payload)
-    return request
+        params=payload
+        )
+    return request.text
 
+# Receives a METAR XML string. Prints each ICAO raw METAR.
 def printMetar(metarXML):
-    metarDict = xmltodict.parse(metarXML.text)
+    metarDict = xmltodict.parse(metarXML)
     data = metarDict["response"]["data"]
-    results = int(data ["@num_results"])
+    results = int(data["@num_results"])
     if results < 1:
         print("No results found.")
     elif results == 1:
-        print(data ["METAR"]["raw_text"])
+        print(data["METAR"]["raw_text"])
     elif results > 1:
         for i in range(results):
-            print(data ["METAR"][i]["raw_text"])
+            print(data["METAR"][i]["raw_text"])
 
 def main():
     needInput = True
