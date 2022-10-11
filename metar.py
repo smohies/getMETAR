191a38a4193eb2f68710db331a017d6e8d82e196
Samuel Martinez <smohuff@gmail.com>
2022-10-11 02:50:49 -0500

    refactor better naming

--- metar.py
+++ metar.py
@@ -4,20 +4,20 @@ import xmltodict
 hours = "1.5"
 
 # Receives a string of 4 character ICAOs separated by commas. Returns a list of ICAOs.
-def transformMetar(metarString) -> list:
-    metarString = metarString.upper().replace(" ", "")
-    metarList = metarString.split(",")
-    for metar in metarList:
+def transformIcao(icaoString) -> list:
+    icaoString = icaoString.upper().replace(" ", "")
+    icaoList = icaoString.split(",")
+    for metar in icaoList:
         if len(metar) != 4:
             return "invalid"
-    return metarList
+    return icaoList
 
 # Receives a list of ICAOs. Returns a METAR XML string.
-def requestMetar(metarList):
+def requestMetar(icaoList):
     payload = {
         "datasource":"metars", "requestType": "retrieve", "format": "xml",
         "mostRecentForEachStation": "constraint", "hoursBeforeNow": hours,
-        "stationString": ",".join(metarList)
+        "stationString": ",".join(icaoList)
         }
     request = requests.get(
         "https://www.aviationweather.gov/adds/dataserver_current/httpparam",
@@ -40,18 +40,18 @@ def printMetar(metarXML):
 
 def main():
     needInput = True
-    metarList = []
+    icaoList = []
     while needInput:
-        metarString = input(
+        icaoString = input(
             "Enter a 4 digit ICAO. If multiple, separate by a comma.\n"
             )
-        metarList = transformMetar(metarString)
-        if metarList == "invalid":
+        icaoList = transformIcao(icaoString)
+        if icaoList == "invalid":
             print("INVALID INPUT, TRY AGAIN")
         else:
             needInput = False
             
-    metarXML = requestMetar(metarList)
+    metarXML = requestMetar(icaoList)
     printMetar(metarXML)
 
 if __name__ == "__main__":
